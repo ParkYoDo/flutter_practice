bd07a38649a14c3402cc253a1aa98742407ed7b0
parkyodo <scarletdemon@naver.com>
2024-09-23 02:17:48 +0900

    feat: restaurant detail screen

---
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -0,0 +1,99 @@
+import 'package:codefactory/common/const/colors.dart';
+import 'package:codefactory/common/const/data.dart';
+import 'package:codefactory/common/layout/default_layout.dart';
+import 'package:codefactory/product/component/product_card.dart';
+import 'package:codefactory/restaurant/component/restaurant_card.dart';
+import 'package:codefactory/restaurant/model/restaurant_detail_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  final String id;
+  final String title;
+
+  const RestaurantDetailScreen(
+      {super.key, required this.id, required this.title});
+
+  Future<Map<String, dynamic>> getRestaurantDetail() async {
+    final dio = Dio();
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final res = await dio.get(
+      'http://$ip/restaurant/$id',
+      options: Options(
+        headers: {'Authorization': 'Bearer $accessToken'},
+      ),
+    );
+
+    return res.data;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        title: title,
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: getRestaurantDetail(),
+          builder: (_, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: PRIMARY_COLOR,
+                ),
+              );
+            }
+
+            final item = RestaurantDetailModel.fromJson(json: snapshot.data!);
+
+            return CustomScrollView(
+              slivers: [
+                renderTop(model: item),
+                renderLabel(),
+                renderProducts(products: item.products)
+              ],
+            );
+          },
+        ));
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts(
+      {required List<RestaurantProductModel> products}) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final model = products[index];
+
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard.fromModel(model: model),
+            );
+          },
+          childCount: products.length,
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop({required RestaurantDetailModel model}) {
+    return SliverToBoxAdapter(
+      child: RestaurantCard.fromModel(
+        model: model,
+        isDetail: true,
+      ),
+    );
+  }
+}
