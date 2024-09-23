bef8e4e33ce3e599d2019ab70319d3a89ec65b3f
parkyodo <scarletdemon@naver.com>
2024-09-24 02:56:53 +0900

    feat: restaurant_repository using retrofit

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -4,6 +4,7 @@ import 'package:codefactory/common/layout/default_layout.dart';
 import 'package:codefactory/product/component/product_card.dart';
 import 'package:codefactory/restaurant/component/restaurant_card.dart';
 import 'package:codefactory/restaurant/model/restaurant_detail_model.dart';
+import 'package:codefactory/restaurant/repository/restaurant_repository.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -14,27 +15,28 @@ class RestaurantDetailScreen extends StatelessWidget {
   const RestaurantDetailScreen(
       {super.key, required this.id, required this.title});
 
-  Future<Map<String, dynamic>> getRestaurantDetail() async {
+  Future<RestaurantDetailModel> getRestaurantDetail() {
     final dio = Dio();
-    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
-    final res = await dio.get(
-      'http://$ip/restaurant/$id',
-      options: Options(
-        headers: {'Authorization': 'Bearer $accessToken'},
-      ),
-    );
+    final repository =
+        RestaurantRepository(dio, baseUrl: 'http://$ip/restaurant');
 
-    return res.data;
+    return repository.getRestaurantDetail(id: id);
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
         title: title,
-        child: FutureBuilder<Map<String, dynamic>>(
+        child: FutureBuilder<RestaurantDetailModel>(
           future: getRestaurantDetail(),
-          builder: (_, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          builder: (_, AsyncSnapshot<RestaurantDetailModel> snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
             if (!snapshot.hasData) {
               return const Center(
                 child: CircularProgressIndicator(
@@ -43,13 +45,11 @@ class RestaurantDetailScreen extends StatelessWidget {
               );
             }
 
-            final item = RestaurantDetailModel.fromJson(snapshot.data!);
-
             return CustomScrollView(
               slivers: [
-                renderTop(model: item),
+                renderTop(model: snapshot.data!),
                 renderLabel(),
-                renderProducts(products: item.products)
+                renderProducts(products: snapshot.data!.products)
               ],
             );
           },
