ee8552a4cbed03b2d88e395b98a3977af6576172
parkyodo <scarletdemon@naver.com>
2024-09-24 13:00:23 +0900

    feat: dio onError interceptor

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -35,4 +35,46 @@ class CustomInterceptor extends Interceptor {
   // 2. 요청을 받을 때
 
   // 3. 에러가 났을 때
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    // 401에러 - 토근 재발급 후 api 재요청
+    print('[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri}');
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    // refreshToken이 없을때 에러 return
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    // token을 갱신하는 api가 아니고 accessToken을 필요로하는 api 요청에서 에러가 났을때
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+
+      try {
+        final res = await dio.post('http://$ip/auth/token',
+            options:
+                Options(headers: {'Authorization': "Bearer $refreshToken"}));
+
+        final accessToken = res.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        // 토근 변경하기
+        options.headers.addAll({'Authorization': 'Bearer $accessToken'});
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        // 요청 재전송
+        final response = await dio.fetch(options);
+
+        return handler.resolve(response);
+      } on DioException catch (e) {
+        return handler.reject(e);
+      }
+    }
+    // refreshToken을 갱신하거나 api status가 401이 아닐때
+    return handler.reject(err);
+  }
 }
