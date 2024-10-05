89d25b1dcef9b10ae41f8eddce51bae225159420
parkyodo <scarletdemon@naver.com>
2024-10-05 20:29:44 +0900

    feat: user_me provider

--- lib/user/provider/user_me_provider.dart
+++ lib/user/provider/user_me_provider.dart
@@ -1,9 +1,22 @@
 import 'package:codefactory/common/const/data.dart';
+import 'package:codefactory/common/secure_storage/secure_storage.dart';
 import 'package:codefactory/user/model/user_model.dart';
 import 'package:codefactory/user/repository/auth_repository.dart';
 import 'package:codefactory/user/repository/user_me_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final userMeProvider =
+    StateNotifierProvider<UserMeStateNotifier, UserModelBase?>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  final userMeRepository = ref.watch(userMeRepositoryProvider);
+  final storage = ref.watch(secureStorageProvider);
+
+  return UserMeStateNotifier(
+      repository: userMeRepository,
+      storage: storage,
+      authRepository: authRepository);
+});
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
   final AuthRepository authRepository;
@@ -62,8 +75,5 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
       storage.delete(key: ACCESS_TOKEN_KEY),
       storage.delete(key: REFRESH_TOKEN_KEY)
     ]);
-
-    // await storage.delete(key: ACCESS_TOKEN_KEY);
-    // await storage.delete(key: REFRESH_TOKEN_KEY);
   }
 }
