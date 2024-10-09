cb199ec824acc4c97ea777ca2fa29fc72c78fb7d
parkyodo <scarletdemon@naver.com>
2024-10-10 01:22:38 +0900

    feat: order repository

--- lib/order/repository/order_repository.dart
+++ lib/order/repository/order_repository.dart
@@ -1,5 +1,8 @@
 import 'package:codefactory/common/const/data.dart';
 import 'package:codefactory/common/dio/dio.dart';
+import 'package:codefactory/common/model/cursor_pagination_model.dart';
+import 'package:codefactory/common/model/pagination_params.dart';
+import 'package:codefactory/common/repository/base_pagination_repository.dart';
 import 'package:codefactory/order/model/order_model.dart';
 import 'package:codefactory/order/model/post_order_body.dart';
 import 'package:dio/dio.dart' hide Headers;
@@ -18,8 +21,16 @@ final orderRepositoryProvider = Provider<OrderRepository>((ref) {
 // baseUrl: http://$ip/order
 
 @RestApi()
-abstract class OrderRepository {
+abstract class OrderRepository
+    implements IBasePaginationRepository<OrderModel> {
   factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @override
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<CursorPagination<OrderModel>> paginate(
+      {@Queries()
+      PaginationParams? paginationPrams = const PaginationParams()});
 
   @POST('/')
   @Headers({'accessToken': 'true'})
