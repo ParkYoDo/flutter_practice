bdb0d71dcd2413137305e66335c72b28d5223414
parkyodo <scarletdemon@naver.com>
2024-10-10 02:01:57 +0900

    feat: apply throttle to pagination api

--- lib/common/provider/pagination_provider.dart
+++ lib/common/provider/pagination_provider.dart
@@ -2,16 +2,34 @@ import 'package:codefactory/common/model/cursor_pagination_model.dart';
 import 'package:codefactory/common/model/model_with_id.dart';
 import 'package:codefactory/common/model/pagination_params.dart';
 import 'package:codefactory/common/repository/base_pagination_repository.dart';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class _PaginationInfo {
+  final int fetchCount;
+  final bool fetchMore;
+  final bool forceRefetch;
+
+  _PaginationInfo(
+      {this.fetchCount = 20,
+      this.fetchMore = false,
+      this.forceRefetch = false});
+}
 
 class PaginationProvider<T extends IModelWithId,
         U extends IBasePaginationRepository<T>>
     extends StateNotifier<CursorPaginationBase> {
   final U repository;
+  final paginationThrottle = Throttle(const Duration(seconds: 3),
+      initialValue: _PaginationInfo(), checkEquality: false);
 
   PaginationProvider({required this.repository})
       : super(CursorPaginationLoading()) {
     paginate();
+
+    paginationThrottle.values.listen((state) {
+      _throttlePagination(state);
+    });
   }
 
   Future paginate({
@@ -24,8 +42,19 @@ class PaginationProvider<T extends IModelWithId,
     // true : CursorPaginationLoading()
     bool forceRefetch = false,
   }) async {
+    paginationThrottle.setValue(_PaginationInfo(
+        fetchCount: fetchCount,
+        fetchMore: fetchMore,
+        forceRefetch: forceRefetch));
+  }
+
+  _throttlePagination(_PaginationInfo info) async {
+    final fetchCount = info.fetchCount;
+    final fetchMore = info.fetchMore;
+    final forceRefetch = info.forceRefetch;
+
     try {
-// 5가지 state
+      // 5가지 state
       // 1. CursorPagination - 정상적으로 데이터 있는 상태
       // 2. CursorPaginationLoading - 데이터 로딩중인 상태 (현재 캐시 없음)
       // 3. CursorPaginationError - 에러 있는 상태
